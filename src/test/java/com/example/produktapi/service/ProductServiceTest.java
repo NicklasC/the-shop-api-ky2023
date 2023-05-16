3846970f3783c0c69541808433349f614a905527
pritut92 <94788040+pritut92@users.noreply.github.com>
2023-05-16 19:51:58 +0200

    TA-44: Created unit test for getAllCategories

--- src/test/java/com/example/produktapi/service/ProductServiceTest.java
+++ src/test/java/com/example/produktapi/service/ProductServiceTest.java
@@ -3,12 +3,16 @@ package com.example.produktapi.service;
 import com.example.produktapi.exception.BadRequestException;
 import com.example.produktapi.model.Product;
 import com.example.produktapi.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -24,6 +28,24 @@ class ProductServiceTest {
     @MockBean
     private ProductRepository repository;
 
+    //Author: PRIYANKA
+    @Test
+    public void getAllCategoriesTest() {
+        String Category1 = "kids clothing";
+        String Category2 = "beauty products";
+
+        List<String> mockCategoryList = new ArrayList<>();
+        mockCategoryList.add(Category1);
+        mockCategoryList.add(Category2);
+
+        when(repository.findAllCategories())
+                .thenReturn(mockCategoryList);
+
+        List<String> categoryList = service.getAllCategories();
+
+        Assertions.assertEquals(2, categoryList.size());
+
+    }
 
     // Author: Jim
     @Test
