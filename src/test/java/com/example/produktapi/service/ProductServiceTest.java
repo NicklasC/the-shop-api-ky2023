54f4cfd2fff211b7b6a7153112e12462d7051994
pritut92 <94788040+pritut92@users.noreply.github.com>
2023-05-16 19:45:07 +0200

    TA-43: Created unit test for getAllProducts

--- src/test/java/com/example/produktapi/service/ProductServiceTest.java
+++ src/test/java/com/example/produktapi/service/ProductServiceTest.java
@@ -3,12 +3,16 @@ package com.example.produktapi.service;
 import com.example.produktapi.exception.BadRequestException;
 import com.example.produktapi.model.Product;
 import com.example.produktapi.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -24,6 +28,23 @@ class ProductServiceTest {
     @MockBean
     private ProductRepository repository;
 
+    //Author: PRIYANKA
+    @Test
+    public void testGetAllProducts() {
+
+        Product productA = new Product("ProductA", 110.10, "jewelery", "beautiful", "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg");
+
+        List<Product> mockProductList = new ArrayList<>();
+        mockProductList.add(productA);
+
+        when(repository.findAll())
+                .thenReturn(mockProductList);
+
+        List<Product> productList = service.getAllProducts();
+
+        Assertions.assertEquals(1, productList.size());
+        Assertions.assertEquals("ProductA", productList.get(0).getTitle());
+    }
 
     // Author: Jim
     @Test
