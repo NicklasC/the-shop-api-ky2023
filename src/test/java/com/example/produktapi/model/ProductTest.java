0d62b370188ec13da463550bc21924d461c25db9
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-05-17 17:40:48 +0200

    TA-54: made code prettier

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class ProductTest {
     Product product;
+
     private Product createTestProduct() {
         Product product = new Product("Golden ring", 25.1, "jewelery", "A beautiful ring", "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg");
         product.setId(25);
@@ -12,89 +13,91 @@ public class ProductTest {
     }
 
     @Test
-    public void testGetId(){
+    public void testGetId() {
         product = createTestProduct();
-        Assert.assertEquals("25",product.getId().toString());
-    }
-    @Test
-    public void testSetId(){
-        product = createTestProduct();
-        product.setId(34);
-        Assert.assertEquals("34",product.getId().toString());
+        Assert.assertEquals("25", product.getId().toString());
     }
 
     @Test
-    public void testGetTitle(){
+    public void testSetId() {
         product = createTestProduct();
-        Assert.assertEquals("Golden ring",product.getTitle());
+        product.setId(34);
+        Assert.assertEquals("34", product.getId().toString());
     }
+
     @Test
-    public void testSetTitle(){
+    public void testGetTitle() {
+        product = createTestProduct();
+        Assert.assertEquals("Golden ring", product.getTitle());
+    }
+
+    @Test
+    public void testSetTitle() {
         product = createTestProduct();
         product.setTitle("New test title");
-        Assert.assertEquals("New test title",product.getTitle());
+        Assert.assertEquals("New test title", product.getTitle());
     }
+
     @Test
-    public void testGetPrice(){
+    public void testGetPrice() {
         product = createTestProduct();
-        Double expectedResult=25.1;
+        Double expectedResult = 25.1;
 
         Double actualResult = product.getPrice();
 
-        Assert.assertEquals(expectedResult,actualResult);
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void testSetPrice(){
+    public void testSetPrice() {
         product = createTestProduct();
         Double newPrice = 54.3;
 
         product.setPrice(newPrice);
         Double actualResult = product.getPrice();
 
-        Assert.assertEquals(newPrice,actualResult);
+        Assert.assertEquals(newPrice, actualResult);
     }
 
     @Test
-    public void testGetCategory(){
+    public void testGetCategory() {
         product = createTestProduct();
-        Assert.assertEquals("jewelery",product.getCategory());
+        Assert.assertEquals("jewelery", product.getCategory());
     }
 
     @Test
-    public void testSetCategory(){
+    public void testSetCategory() {
         product = createTestProduct();
         product.setCategory("Test category");
-        Assert.assertEquals("Test category",product.getCategory());
+        Assert.assertEquals("Test category", product.getCategory());
     }
 
 
     @Test
-    public void testGetDescription(){
+    public void testGetDescription() {
         product = createTestProduct();
-        Assert.assertEquals("A beautiful ring",product.getDescription());
+        Assert.assertEquals("A beautiful ring", product.getDescription());
     }
 
     @Test
-    public void testSetDescription(){
+    public void testSetDescription() {
         product = createTestProduct();
         product.setDescription("This is a new description");
-        Assert.assertEquals("This is a new description",product.getDescription());
+        Assert.assertEquals("This is a new description", product.getDescription());
     }
 
     @Test
-    public void testGetImage(){
+    public void testGetImage() {
         product = createTestProduct();
-        Assert.assertEquals("https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",product.getImage());
+        Assert.assertEquals("https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg", product.getImage());
     }
 
     @Test
-    public void testSetImage(){
+    public void testSetImage() {
         product = createTestProduct();
         product.setImage("https://someurl.com/test.jpg");
-        Assert.assertEquals("https://someurl.com/test.jpg",product.getImage());
+        Assert.assertEquals("https://someurl.com/test.jpg", product.getImage());
     }
-
 
 
 }
