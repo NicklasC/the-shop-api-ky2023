ed63adebd1e0a34f67a05e5f8da5c8d1a1b0a72f
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-05-18 14:58:18 +0200

    TA-47: Add test för EntityNotFound (and also bad request).

---
+++ src/test/java/com/example/produktapi/exception/EntityNotFoundExceptionTest.java
@@ -0,0 +1,49 @@
+package com.example.produktapi.exception;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public class EntityNotFoundExceptionTest {
+    //@LocalServerPort
+    @LocalServerPort
+    private Integer port;
+
+    // Author: Nicklas
+    @Test
+    void test_EntityNotFound() {
+        String expectedProductId = "555";
+        String expectedResponse = "Produkt med id " + expectedProductId + " hittades inte";
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("http://localhost:" + port + "/products" + "/" + expectedProductId)
+                .then()
+                .extract().
+                response();
+        Assertions.assertEquals(expectedResponse, response.asString());
+        Assertions.assertEquals(404, response.getStatusCode());
+    }
+
+    // Author: Nicklas
+    @Test
+    void test_BadRequest() {
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("http://localhost:" + port + "/products" + "/ghghh")
+                .then()
+                .extract().
+                response();
+
+        Assertions.assertEquals(400, response.getStatusCode());
+    }
+}
