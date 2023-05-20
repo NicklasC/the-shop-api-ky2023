43a55ec164dfecd3a04bc34aea3caba385b5e50e
JimW <132007431+JimWAGTE@users.noreply.github.com>
2023-05-20 10:54:51 +0200

    Update ProductServiceTest.java
    Handling merge conflict.

--- src/test/java/com/example/produktapi/service/ProductServiceTest.java
+++ src/test/java/com/example/produktapi/service/ProductServiceTest.java
@@ -12,24 +12,77 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ProductServiceTest {
+
     @Autowired
     private ProductService service;
+
     @MockBean
     private ProductRepository repository;
 
     //Author: PRIYANKA
     @Test
-    void testDeleteProductThrowException() {
+    public void getAllCategoriesTest() {
+        String Category1 = "kids clothing";
+        String Category2 = "beauty products";
 
-        assertThrows(EntityNotFoundException.class, () -> {
-            service.deleteProduct(35);
+        List<String> mockCategoryList = new ArrayList<>();
+        mockCategoryList.add(Category1);
+        mockCategoryList.add(Category2);
+
+        when(repository.findAllCategories())
+                .thenReturn(mockCategoryList);
+
+        List<String> categoryList = service.getAllCategories();
+
+        Assertions.assertEquals(2, categoryList.size());
+    }
+    
+    //Author: PRIYANKA
+    @Test
+    public void testGetAllProducts() {
+
+        Product productA = new Product("ProductA", 110.10, "jewelery", "beautiful", "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg");
+
+        List<Product> mockProductList = new ArrayList<>();
+        mockProductList.add(productA);
+
+        when(repository.findAll())
+                .thenReturn(mockProductList);
+
+        List<Product> productList = service.getAllProducts();
+
+        Assertions.assertEquals(1, productList.size());
+        Assertions.assertEquals("ProductA", productList.get(0).getTitle());
+    }
+
+    // Author: Jim
+    @Test
+    void test_addProduct() {
+        Product mockHatProduct = new Product("Green hat", 10.0, "Hats", "A green hat", "http://imagelink");
+        when(repository.save(mockHatProduct))
+                .thenReturn(mockHatProduct);
+        assertEquals(mockHatProduct, service.addProduct(mockHatProduct));
+    }
+
+    // Author: Jim
+    @Test
+    void test_addProductException() {
+        Product mockProduct = mock(Product.class);
+        when(repository.save(mockProduct))
+                .thenThrow(BadRequestException.class);
+        assertThrows(BadRequestException.class, () -> {
+            service.addProduct(mockProduct);
         });
     }
 
@@ -66,5 +119,14 @@ class ProductServiceTest {
 
         verify(repository, times(1)).deleteById(5);
 
+    }
+    
+    //Author: PRIYANKA
+    @Test
+    void testDeleteProductThrowException() {
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            service.deleteProduct(35);
+        });
     }
 }
