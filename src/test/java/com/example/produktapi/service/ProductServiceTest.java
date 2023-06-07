6656d219079529c754989a29622b48ac3667b593
NishiyamaStudios <jcwasterberg@gmail.com>
2023-06-07 19:40:34 +0200

    changed localhost to new URL

--- src/test/java/com/example/produktapi/service/ProductServiceTest.java
+++ src/test/java/com/example/produktapi/service/ProductServiceTest.java
@@ -7,7 +7,6 @@ import com.example.produktapi.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
